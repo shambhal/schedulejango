eff86ec3c674cad9cf21f073cff4fc228d375afa
shambhal <shambhalnetworks@gmail.com>
2026-02-03 20:34:53 +0530

    jello

--- appoint/settings.py
+++ appoint/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env("SECRET_KEY",default="django-insecure-jd$y&!87ppw8l#nrz+*12x93t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =env("DEBUG")
 
-DEBUG=True
+#DEBUG=True
 
 
 
@@ -43,16 +43,16 @@ ALLOWED_HOSTS = [
     if h.strip()
 ]
 
-'''MEDIA_URL = "/schedule/media/"
+MEDIA_URL = "/schedule/media/"
 
-'''
-MEDIA_URL = "/media/"
+
+#MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 MEDIA_ICACHE = os.path.join(BASE_DIR, "media/cache/")
 MEDIA_STATIC = os.path.join(BASE_DIR, "media/")
 ICACHE_URL = MEDIA_IURL = "/schedule/media/cache/"
-ICACHE_URL = MEDIA_IURL = "/media/cache/"
+#ICACHE_URL = MEDIA_IURL = "/media/cache/"
 # Application definition
 
 INSTALLED_APPS = [
